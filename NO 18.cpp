1b1ac6f4d3144046101a49a62e1d80f8d0e11d4b
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2025-04-20 23:57:07 +0500

    Array

---
+++ NO 18.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b,c;
+	c=1;
+	for(a=1;a<=5;a++)
+	{
+	  for(b=1;b<=5;b++)
+	    {
+	      cout<<c<<"\t";
+	      c++;
+        }
+	 cout<<endl; 
+    }
+return 0;
+}
